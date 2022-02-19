62a066dea16388f6a27d48e1beb8598c0e841c08
gaga1313 <gauravgaonkar13.13@gmail.com>
2022-02-19 16:58:35 +0530

    I was here

--- researchpanacea/backend/models.py
+++ researchpanacea/backend/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# I was  here 
 class Conference(models.Model):
     name = models.CharField(max_length=500)
     date = models.IntegerField()
