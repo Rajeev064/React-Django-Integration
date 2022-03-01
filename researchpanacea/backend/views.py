a05215cb1f9206a31302ecfa9e36c5aa2be0cdf5
gaga1313 <gauravgaonkar13.13@gmail.com>
2022-03-01 12:07:33 +0530

    'recommendationpseudo'

--- researchpanacea/backend/views.py
+++ researchpanacea/backend/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+import numpy as np
+from ResearchPanacea.researchpanacea.backend.models import Conference
+
 
 # Create your views here.
+@csrf_exempt
+def recommendation(request, id = 0):
+    if request.method == 'GET':
+        #sql query
+        li = []
+        user_id = li[0]
+        user_info = li[1:]
+        corr_score = np.dot(user_info , user_info)
+        idx = user_id[id]
+        output = corr_score[idx]
+        return JsonResponse(output)
+
+    
